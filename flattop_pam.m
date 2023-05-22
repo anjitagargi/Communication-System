f5887c15df8646908f950edb83ba78d5de811d02
Anjita Gargi Chandora <91597725+anjitagargi@users.noreply.github.com>
2023-05-22 23:38:05 +0530

    Create flattop_pam.m

---
+++ flattop_pam.m
@@ -0,0 +1,30 @@
+clc;
+fc = 20;
+fm =2;
+fs = 1000;
+t=1;
+n = [0:1/fs:t];
+n = n(1:end-1);
+dutycycle = 50;
+s = square(2*pi*fc*n,dutycycle);
+s(find(s<0))=0;   
+m = sin(2*pi*fm*n);
+period_sam = length(n)/fc;    
+ind = 1:period_sam:length(n);   
+on_samp = ceil(period_sam * dutycycle/100);   
+pam = zeros(1,length(n));
+for i =1:length(ind)
+    pam(ind(i):ind(i)+on_samp) = m(ind(i));
+end
+subplot(3,1,1);
+plot(n,s);
+ylim([-0.2 1.2]);
+title("Impulse Signal");
+subplot(3,1,2);
+plot(n,m);
+ylim([-1.2 1.2]);
+title("Message Signal");
+subplot(3,1,3);
+plot(n,pam);
+ylim([-1.2 1.2]);
+title("PAM Signal");
